282b92e4827dd5ced775ad59aff8d59bfca1568a
BuildTools <unconfigured@null.spigotmc.org>
2024-05-25 08:28:52 +0200

    Add permission methode

--- src/main/java/at/leisner/server/command/Command.java
+++ src/main/java/at/leisner/server/command/Command.java
@@ -3,7 +3,8 @@ package at.leisner.server.command;
 import java.util.List;
 
 public abstract class Command {
-    private final String name;
+    protected final String name;
+    private String permission = null;
 
     public Command(String name) {
         this.name = name;
@@ -13,9 +14,17 @@ public abstract class Command {
         return name;
     }
 
-    public abstract void execute(String[] args);
+    public abstract void execute(String label, String[] args);
 
-    public List<String> tabComplete(String[] args) {
+    public List<String> tabComplete(String label, String[] args) {
         return null;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    protected void setPermission(String permission) {
+        this.permission = permission;
     }
 }
