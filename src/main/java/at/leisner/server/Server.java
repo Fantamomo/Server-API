abd97f500dc5372470449f57790d5c80e0ff8448
BuildTools <unconfigured@null.spigotmc.org>
2024-05-25 08:40:06 +0200

    Add getConnectedClientNumberSinceStart and getUserManager methode

--- src/main/java/at/leisner/server/Server.java
+++ src/main/java/at/leisner/server/Server.java
@@ -4,10 +4,13 @@ import at.leisner.server.command.CommandManager;
 import at.leisner.server.event.EventManager;
 import at.leisner.server.plugin.JavaPlugin;
 import at.leisner.server.plugin.PluginManager;
+import at.leisner.server.user.PermissionManager;
 
 public interface Server {
     PluginManager getPluginmanager();
     EventManager getEventManager();
     JavaPlugin getPlugin(String id);
     CommandManager getCommandManager();
+    int getConnectedClientNumberSinceStart();
+    PermissionManager getUserManager(JavaPlugin javaPlugin);
 }
