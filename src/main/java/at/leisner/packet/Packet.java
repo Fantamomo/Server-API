304539f0a0ea67f815d712d10c07754ede1a71af
BuildTools <unconfigured@null.spigotmc.org>
2024-05-25 08:36:08 +0200

    Updating Methode Parameter for MesssagePacket

--- src/main/java/at/leisner/packet/Packet.java
+++ src/main/java/at/leisner/packet/Packet.java
@@ -3,25 +3,25 @@ package at.leisner.packet;
 import java.io.Serial;
 import java.io.Serializable;
 
-public record Packet(PacketType packetType, CustomPacket... objects) implements Serializable {
+public record Packet(PacketType packetType, MessagePacket messagePacket, CustomPacket... objects) implements Serializable {
     @Serial
     private static final long serialVersionUID = 29092009L;
 
-    public static Packet getMessagePacket(CustomPacket... packetObjects) {
-        return new Packet(PacketType.MESSAGE, packetObjects);
+    public static Packet getMessagePacket(MessagePacket messagePacket, CustomPacket... packetObjects) {
+        return new Packet(PacketType.MESSAGE, messagePacket, packetObjects);
     }
 
-    public static Packet getPrivateMessagePacket(CustomPacket... packetObjects) {
-        return new Packet(PacketType.MESSAGE_PRIVATE, packetObjects);
+    public static Packet getPrivateMessagePacket(MessagePacket messagePacket, CustomPacket... packetObjects) {
+        return new Packet(PacketType.MESSAGE_PRIVATE, messagePacket, packetObjects);
     }
 
     public static Packet getClientTypePacket(ClientTypePacket type) {
-        return new Packet(PacketType.CLIENT_TYPE, type);
+        return new Packet(PacketType.CLIENT_TYPE, MessagePacket.of("CLIENT_TYPE"), type);
     }
     public static Packet getDisconnectPacket() {
-        return new Packet(PacketType.DISCONNECT, PacketObject.of(DisconnectReason.UNKNOWN));
+        return new Packet(PacketType.DISCONNECT, MessagePacket.of("DISCONNECT"),PacketObject.of(DisconnectReason.UNKNOWN));
     }
     public static Packet getDisconnectPacket(DisconnectReason disconnectReason) {
-        return new Packet(PacketType.DISCONNECT, PacketObject.of(disconnectReason));
+        return new Packet(PacketType.DISCONNECT, MessagePacket.of("DISCONNECT_REASON"), PacketObject.of(disconnectReason));
     }
 }
