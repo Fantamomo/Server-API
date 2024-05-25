6befe68ba1ca2576bbf8fd9a2b5958bc91331ec4
BuildTools <unconfigured@null.spigotmc.org>
2024-05-25 08:34:40 +0200

    Update Logic of class.

--- src/main/java/at/leisner/server/plugin/JavaPlugin.java
+++ src/main/java/at/leisner/server/plugin/JavaPlugin.java
@@ -1,12 +1,20 @@
 package at.leisner.server.plugin;
 
+import at.leisner.packet.Packet;
 import at.leisner.server.Server;
 
+import java.io.File;
+import java.io.ObjectInputStream;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public abstract class JavaPlugin {
     private Server fantaServerServer;
     private Logger fantaServerLogger;
+    private File fantaServerDirectory;
+    private ClassLoader fantaServerClassLoader;
+    protected boolean debug = true;
+    protected String[] allowedPackets;
 
     public final Server getServer() {
         return fantaServerServer;
@@ -20,5 +28,26 @@ public abstract class JavaPlugin {
     }
     public final Logger getLogger() {
         return fantaServerLogger;
+    }
+    public final File getPluginDirectory() {
+        return fantaServerDirectory;
+    }
+    protected final boolean makePluginDirectory() {
+        return getPluginDirectory().mkdir();
+    }
+    public Packet getPacket(ObjectInputStream objectInputStream) {
+        return null;
+    }
+
+    public ClassLoader getClassLoader() {
+        return fantaServerClassLoader;
+    }
+    public void debug(String msg) {
+        if (!debug) return;
+        getLogger().log(Level.CONFIG, msg);
+    }
+
+    public String[] getAllowedPackets() {
+        return allowedPackets;
     }
 }
